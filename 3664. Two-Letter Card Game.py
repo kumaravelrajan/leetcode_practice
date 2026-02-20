a3a1b33350c3b78b6fd4290d86bb60347d606d42
Kumaravel Rajan <kumaravel.rajan7@gmail.com>
2026-02-20 16:36:25 +0100

    two letter card game

--- 3664. Two-Letter Card Game.py
+++ 3664. Two-Letter Card Game.py
@@ -3,71 +3,76 @@ from collections import defaultdict
 
 class Solution:
 
-    def get_pairs_prefix_or_suffix_with_both(pos1or2_lookup, both_pos_lookup):
-        score = 0
-
-        
-
     def score(self, cards: List[str], x: str) -> int:
-        x_in_pos1_lookup = defaultdict(int)
-        x_in_pos2_lookup = defaultdict(int)
-        x_in_both_pos_lookup = defaultdict(int)
-        score = 0
-        balance = None
+
+        set1 = defaultdict(int)
+        tset1 = 0
+        freqset1 = 0
+        set2 = defaultdict(int)
+        tset2 = 0
+        freqset2 = 0
+        tbothx = 0
+        answer = 0
+        used = 0
 
         for card in cards:
-            if card[0] == x and card[1] == x:
-                x_in_both_pos_lookup[x] += 1
+            if card[0] == x and card[1] != x:
+                tset1 += 1
+                set1[card[1]] += 1
+
+                if freqset1 < set1[card[1]]:
+                    freqset1 = set1[card[1]]
             
-            elif card[0]==x and card[1] != x:
-                x_in_pos1_lookup[card[1]] += 1
-
             elif card[0] != x and card[1] == x:
-                x_in_pos2_lookup[card[0]] += 1
+                tset2 += 1
+                set2[card[0]] += 1
 
-        for key, value in x_in_pos1_lookup.items():
-            if balance is None:
-                balance = value
-            else:
-                score += min(balance, value)
-                balance = abs(balance - value)
+                if freqset2 < set2[card[0]]:
+                    freqset2 = set2[card[0]]
+            
+            elif card[0] == x and card[1] == x:
+                tbothx += 1
 
-        # If there are no cards such as "xy" to load into pos1_lookup initialize balance to 0
-        if balance is None:
-            balance = 0
 
-        # If there are still cards remaining s.t. 'xy' for some y then check if there exist any cards in x_in_both_pos_lookup so that these can be paired up. 
-        if balance > 0:
-            if x_in_both_pos_lookup:
-                if balance > x_in_both_pos_lookup[x]:
-                    score += x_in_both_pos_lookup[x]
-                    x_in_both_pos_lookup[x] = 0
-                
-                else:
-                    score += balance
-                    x_in_both_pos_lookup[x] = x_in_both_pos_lookup[x] - balance
+        # Pairing cards in same set
+        if freqset1 > tset1 // 2:
+            answer += tset1 - freqset1
+            tset1 = tset1 - (2 * (tset1 - freqset1))
 
-        # Now focus on pos2_lookup
-        balance = None # Because balance of pos1_lookup worthless in pos2_lookup since no card in pos1_lookup can be compatible with any card in pos2_lookup. 
+        elif freqset1 <= tset1 // 2:
+            answer += tset1 // 2
+            tset1 = tset1 % 2
 
-        for key, value in x_in_pos2_lookup.items():
-            if balance is None:
-                balance = value
-            else:
-                score += min(balance, value)
-                balance = abs(balance - value)
+        if freqset2 > tset2 // 2:
+            answer += tset2 - freqset2
+            tset2 = tset2 - (2 * (tset2 - freqset2))
 
-        if balance is None: 
-            # If there was no entry in pos2_lookup, balance never changed from None. Hence initialize it to 0.
-            balance = 0
+        elif freqset2 <= tset2:
+            answer += tset2 // 2
+            tset2 = tset2 % 2
 
-        if balance > 0:
-            if x_in_both_pos_lookup[x] > 0:
-                # This means there are still unused cards in both_pos_lookup which can be paired with leftovers from pos2_lookup
+        #Pairing leftover cards in set1 (and set2) with cards in double_x.
 
-                score += min(balance, x_in_both_pos_lookup[x])
+        while tset1 != 0 and tbothx != 0:
+            tset1 -= 1
+            tbothx -= 1
+            used += 1
 
-        return score
+        while tset2 != 0 and tbothx != 0:
+            tset2 -= 1
+            tbothx -= 1
+            used += 1
+
+        # Spitting pairs in answer if pairs exist in tbothx so that one additional pair can be created
+
+        while answer > 0 and tbothx > 1:
+            answer -= 1
+            tbothx -= 2
+            used += 2
+
+        return answer + used
+
+        
 
 sol = Solution()
 print(sol.score(["aa", "ab", "ba", "ac"], "a"))
